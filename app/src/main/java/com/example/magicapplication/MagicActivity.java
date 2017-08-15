14b5c80705e63b0bab138b9e67df82228e5c0de0
Guy Bachkhaznadji <guysergebach@hotmail.com>
2017-08-15 13:40:52 +0100

    added a responsive grid to my MagicActivity

---
+++ app/src/main/java/com/example/magicapplication/MagicActivity.java
@@ -0,0 +1,13 @@
+package com.example.magicapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MagicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_magic);
+    }
+}
